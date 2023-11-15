5df4d642f44254d5bbf65df155c64d2a2aa0123f
Patrick Augusto <ptkato.irl@gmail.com>
2023-11-15 14:59:17 -0300

    Show only active listings

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -11,6 +11,7 @@ import Text.Hamlet
 
 getHomeR :: Handler Html
 getHomeR = do
-  pages <- runDB $ selectPaginated 10 [] [Asc ListingTitle]
+  amount <- runDB $ count [ListingActive ==. True]
+  pages  <- runDB $ selectPaginated 10 [] [Asc ListingTitle]
 
   defaultLayout $(whamletFile "templates/home.hamlet")
