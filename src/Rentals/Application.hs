666f2881b862c106810438a51efb98b997cca41c
Jappie Klooster <jappieklooster@hotmail.com>
2023-12-24 00:32:12 +0100

    use better optional settings

--- src/Rentals/Application.hs
+++ src/Rentals/Application.hs
@@ -1,64 +1,58 @@
-{-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE TemplateHaskell      #-}
-{-# LANGUAGE ViewPatterns         #-}
-
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- | This hooks everything together
 module Rentals.Application where
 
-import Rentals.Database.Import
+import Control.Concurrent
+import Control.Concurrent.Thread.Delay
+import Control.Lens (to, (^.))
+import Control.Monad
+import Control.Monad.Logger (runStderrLoggingT)
+import Control.Monad.Trans.Resource (runResourceT)
+import Data.Default (def)
+import Data.Functor
+import Data.List (uncons)
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Traversable
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Network.HTTP.Types.Method
+import Network.URI
+import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
+import qualified Network.Wreq as W
+import Rentals.Database (migrateAll)
 import Rentals.Database.Event
+import Rentals.Database.Import
 import Rentals.Foundation
-import Yesod
-import Yesod.Auth
-import Yesod.Default.Config2
-
-import           Control.Concurrent
-import           Control.Concurrent.Thread.Delay
-import           Control.Lens                    ((^.), to)
-import           Control.Monad
-import           Control.Monad.Logger            (runStderrLoggingT)
-import           Control.Monad.Trans.Resource    (runResourceT)
-import           Data.Default                    (def)
-import           Data.Functor
-import qualified Data.Map.Strict                 as M
-import qualified Data.Text.Lazy                  as LT
-import qualified Data.Text                  as T
-import           Data.Traversable
-import           Database.Persist
-import           Database.Persist.TH
-import           Database.Persist.Sqlite
-import           Network.HTTP.Types.Method
-import           Network.URI
-import           Network.Wai.Handler.Warp
-import           Network.Wai.Middleware.Cors
-import qualified Network.Wreq                    as W
-import           System.Directory
-import           Text.ICalendar
-
-import Rentals.Database(migrateAll)
-import Rentals.Handler.Admin.Listing
 import Rentals.Handler.Admin.Export
 import Rentals.Handler.Admin.Import
+import Rentals.Handler.Admin.Listing
 import Rentals.Handler.User.Booking
 import Rentals.Handler.User.Listing
 import Rentals.Handler.User.User
 import Rentals.Handler.View.Admin
 import Rentals.Handler.View.Listings
-
 import Rentals.Settings
-import Control.Concurrent.Async(async)
+import System.Directory
+import Text.ICalendar
+import Yesod
+import Yesod.Auth
+import Yesod.Default.Config2
 
 mkYesodDispatch "App" resourcesApp
 
 appMain :: IO ()
 appMain = do
-
-  configPath <- bifold <$> race ("config/settings.yml" <$ threadDelay 10_000) getLine
+  configPath <- fromMaybe "config/settings.yml" . fmap fst . uncons <$> getArgs
   putStrLn $ "reading config from: " <> configPath
-  settings <- loadYamlSettings [configPath ] [] useEnv
+  settings <- loadYamlSettings [configPath] [] useEnv
 
   createDirectoryIfMissing True "images"
 
@@ -74,23 +68,32 @@ appMain = do
           ics <- liftIO . W.get $ (uriToString id uri) ""
 
           case parseICalendar def parseErrorLog $ ics ^. W.responseBody of
-            Right (ical:_, _) -> do
+            Right (ical : _, _) -> do
               void . flip M.traverseWithKey (vcEvents ical) $ \_ e -> do
-                let uuid        = LT.toStrict . uidValue $ veUID e
-                    mdates      = case (veDTStart e, veDTEndDuration e) of
+                let uuid = LT.toStrict . uidValue $ veUID e
+                    mdates = case (veDTStart e, veDTEndDuration e) of
                       (Just (DTStartDate (Date start) _), Just (Left (DTEndDate (Date end) _))) -> Just (start, end)
                       (Just (DTStartDate (Date start) _), _) -> Just (start, start)
                       _ -> Nothing
                     description = fmap (LT.toStrict . descriptionValue) $ veDescription e
-                    summary     = fmap (LT.toStrict . summaryValue) $ veSummary e
+                    summary = fmap (LT.toStrict . summaryValue) $ veSummary e
 
                 case mdates of
                   Just (start, end) -> do
                     void . for [start .. end] $ \start' ->
-                      insertBy $ Event cid source uuid
-                        start' end Nothing description summary False True
+                      insertBy $
+                        Event
+                          cid
+                          source
+                          uuid
+                          start'
+                          end
+                          Nothing
+                          description
+                          summary
+                          False
+                          True
                   Nothing -> pure ()
-
             Left err ->
               $logError $ "Parsing ical failed: " <> T.pack err
 
@@ -98,12 +101,16 @@ appMain = do
 
     waiApp <- toWaiApp (App settings pool)
     run (appPort settings) $
-      (cors $ \req -> Just $
-        simpleCorsResourcePolicy
-          {corsMethods =
-            [ methodOptions
-            , methodGet
-            , methodPost
-            , methodPut
-            , methodDelete
-            ]}) waiApp
+      ( cors $ \req ->
+          Just $
+            simpleCorsResourcePolicy
+              { corsMethods =
+                  [ methodOptions,
+                    methodGet,
+                    methodPost,
+                    methodPut,
+                    methodDelete
+                  ]
+              }
+      )
+        waiApp
