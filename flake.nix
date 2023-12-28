96375bbe51123880e63c2260bc8f7152288535ae
Jappie Klooster <jappieklooster@hotmail.com>
2023-12-28 09:03:56 +0100

    jailbreak

--- flake.nix
+++ flake.nix
@@ -32,6 +32,8 @@
                      url = "https://github.com/ptkato/iCalendar";
                      rev = "f96cd2bc84313fb25f3581f8d31c437cd855f1d7";
                   }) {});
+          base64-bytestring = lib.doJailbreak hold.base64-bytestring;
+          bytestring = lib.doJailbreak hold.bytestring;
         };
       };
     in
